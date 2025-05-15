b7577130bebe27a533933d2d420dc7fbedcb917e
MrSuicideFish <malcolm259@gmail.com>
2025-05-15 01:13:28 -0400

    player controller

--- Animail-Unity/Assets/Scripts/Player/PlayerController.cs
+++ Animail-Unity/Assets/Scripts/Player/PlayerController.cs
@@ -4,56 +4,69 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Transform m_view;
-    [SerializeField] private Rigidbody m_rigidBody;
+    [SerializeField] private CharacterController m_characterController;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float gravity = -9.8f;
+    [SerializeField] private float glideGravity = -2f;
+    [SerializeField] private float fallMultiplier = 2.5f;
+
     private Vector2 m_moveDirection;
-    private bool m_isFacingRight;
-    private bool m_hasJumped;
+    private Vector3 m_velocity;
     private bool m_isGrounded;
-    
-    public float MoveSpeed = 5f;
-    public float ViewRotationSpeed = 0.1f;
+    private bool m_isJumping;
+    private bool m_isGliding;
 
     private void Update()
     {
-        // flip the player around when changing direction
-        if (m_moveDirection.x != 0)
+        // Check if the player is grounded
+        m_isGrounded = m_characterController.isGrounded;
+
+        if (m_isGrounded && m_velocity.y < 0)
         {
-            m_isFacingRight = !(m_moveDirection.x < 0);
+            m_velocity.y = 0f; // Reset vertical velocity when grounded
         }
 
-        Vector3 targetRot = new Vector3(0, m_isFacingRight ? 0 : 180, 0);
-        m_view.rotation = Quaternion.Slerp(m_view.rotation, Quaternion.Euler(targetRot), ViewRotationSpeed * Time.deltaTime);
+        // Horizontal movement
+        Vector3 move = new Vector3(m_moveDirection.x, 0, m_moveDirection.y);
+        m_characterController.Move(move * moveSpeed * Time.deltaTime);
 
-        if (m_hasJumped)
+        // Apply gravity
+        if (m_isGliding)
         {
-            //velocity.y = 10;
+            m_velocity.y += glideGravity * Time.deltaTime;
         }
-        
-        if (m_isGrounded)
+        else if (!m_isGrounded && !m_isJumping)
         {
+            m_velocity.y += gravity * fallMultiplier * Time.deltaTime;
+        }
+        else
+        {
+            m_velocity.y += gravity * Time.deltaTime;
+        }
 
-        }
-        
-        Vector3 move = transform.right * m_moveDirection.x + transform.forward * m_moveDirection.y;
-        m_rigidBody.AddForce(move * MoveSpeed, ForceMode.VelocityChange);
+        // Apply vertical velocity
+        m_characterController.Move(m_velocity * Time.deltaTime);
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if (context.started && m_isGrounded)
         {
-            m_hasJumped = true;
-        }else if(context.canceled)
+            m_isJumping = true;
+            m_velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity); // Apply jump force
+        }
+        else if (context.canceled)
         {
-            m_hasJumped = false;
+            m_isJumping = false;
+            m_isGliding = false; // Stop gliding when jump is released
+        }
+        else if (context.performed && !m_isGrounded)
+        {
+            m_isGliding = true; // Start gliding when holding jump
         }
     }
 
-    public void OnInteract()
-    {
-        
-    }
-    
     public void OnMove(InputAction.CallbackContext context)
     {
         m_moveDirection = context.ReadValue<Vector2>();
