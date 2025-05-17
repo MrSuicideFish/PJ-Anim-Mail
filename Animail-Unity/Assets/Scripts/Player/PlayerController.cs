cf43b234baad4269b866aa807212e8e48f9d0aa0
MrSuicideFish <malcolm259@gmail.com>
2025-05-17 14:35:24 -0400

    Added tileset stuff

--- Animail-Unity/Assets/Scripts/Player/PlayerController.cs
+++ Animail-Unity/Assets/Scripts/Player/PlayerController.cs
@@ -3,25 +3,23 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private Transform m_view;
-    [SerializeField] private CharacterController m_characterController;
-    [SerializeField] private Animator m_animator;
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float jumpForce = 10f;
-    [SerializeField] private float gravity = -9.8f;
-    [SerializeField] private float glideGravity = -2f;
-    [SerializeField] private float fallMultiplier = 2.5f;
-    [SerializeField] private float ViewRotationSpeed = 2.5f;
+    [Header("Components")] 
+    public Rigidbody2D m_rigidBody;
+    public Transform m_view;
+    
+    [Header("Sprite")]
+    public float ViewRotationSpeed = 5.0f;
+    
+    [Header("Ground Check")]
+    public Vector3 m_checkHeightOffset;
+    public float m_checkDistance = 1.0f;
 
-    private Vector2 m_moveDirection;
-    private Vector3 m_velocity;
+    private Vector3 m_moveDirection;
     private bool m_isGrounded;
     private bool m_isJumping;
     private bool m_isGliding;
-    private bool m_isFacingRight = true;
-    private float m_jumpReleaseTime;
-    private float m_glideTime;
-
+    private bool m_isFacingRight;
+    
     private void HandleFacingDirection()
     {
         // Flip the player around when changing direction
@@ -34,45 +32,17 @@ public class PlayerController : MonoBehaviour
         m_view.rotation = Quaternion.Slerp(m_view.rotation, Quaternion.Euler(targetRot), ViewRotationSpeed * Time.deltaTime);
     }
 
+    private void CheckGrounded()
+    {
+        Ray ray = new Ray(transform.position + m_checkHeightOffset, Vector3.down);
+        m_isGrounded = Physics.Raycast(ray, out RaycastHit hit, m_checkDistance, LayerMask.GetMask("Default"));
+    }
+
     private void Update()
     {
         InputSystem.Update();
+        CheckGrounded();
         HandleFacingDirection();
-        
-        m_isGrounded = m_characterController.isGrounded;
-        
-        // horizontal movement
-        Vector3 move = new Vector3(m_moveDirection.x, 0, m_moveDirection.y);
-        m_characterController.Move(move * moveSpeed * Time.deltaTime);
-        
-        // Check for jump peak and enable gliding if holding jump
-        if (!m_isGrounded && m_velocity.y <= 0 && m_isJumping)
-        {
-            m_isGliding = true;
-        }
-        
-        // Apply gravity
-        if (m_isGliding)
-        {
-            m_glideTime = Mathf.Clamp01(m_glideTime + Time.deltaTime);
-            m_velocity.y += glideGravity * Time.deltaTime * (1.0f + (m_glideTime * (fallMultiplier * 2.0f)));
-        }
-        else if (!m_isGrounded && !m_isJumping)
-        {
-            m_jumpReleaseTime = Mathf.Clamp01(m_jumpReleaseTime + Time.deltaTime * fallMultiplier);
-            float multiplier = fallMultiplier * m_jumpReleaseTime;
-            m_velocity.y += gravity * multiplier * Time.deltaTime;
-        }
-        else
-        {
-            m_velocity.y += gravity * Time.deltaTime;
-        }
-
-        m_characterController.Move(m_velocity * Time.deltaTime);
-
-        m_animator.SetFloat("Velocity", move.normalized.magnitude);
-        m_animator.SetBool("IsGrounded", m_isGrounded);
-        m_animator.SetTrigger("hasJumped");
     }
 
     public void OnJump(InputAction.CallbackContext context)
@@ -83,7 +53,6 @@ public class PlayerController : MonoBehaviour
             {
                 m_isJumping = true;
                 m_isGliding = false;
-                m_velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
             }
             else
             {
@@ -92,7 +61,6 @@ public class PlayerController : MonoBehaviour
         }
         else if (context.canceled)
         {
-            m_jumpReleaseTime = 0.0f;
             m_isGliding = false;
             m_isJumping = false;
         }
